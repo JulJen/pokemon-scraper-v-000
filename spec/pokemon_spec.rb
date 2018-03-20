12a8033cdcda6d7f65a54051bfa789596ed3ba26
Jul Jen <juliajen.prog@gmail.com>
2018-03-20 22:26:21 +0000

    automatically backed up by learn

--- spec/pokemon_spec.rb
+++ spec/pokemon_spec.rb
@@ -43,7 +43,7 @@ describe "Pokemon" do
   describe "BONUS" do
 
     before do
-      @sql_runner.execute_alter_hp_migration.sql
+      @sql_runner.execute_alter_hp_migration
       Pokemon.save('Pikachu', 'electric', @db)
       Pokemon.save('Magikarp', 'water', @db)
     end
