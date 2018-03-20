ff1a1d52c8e9442999802d2696ee7e70def72fb1
Jul Jen <juliajen.prog@gmail.com>
2018-03-20 22:22:29 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -28,20 +28,9 @@ class Pokemon
     pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", id).first
 # binding.pry
     self.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], hp: pokemon[3], db: db)
-    #  hp: pokemon[3])
-    # @db=nil, @hp=nil, @id=nil, @name=nil, @type=nil>
-    # ("Pikachu", "electric", @db)
   end
-end
 
-# describe ".find" do
-#   it 'finds a pokemon from the database by their id number and returns a new Pokemon object' do
-#     # The find method creates a new Pokemon after selecting their row from the database by their id number.
-#     Pokemon.save("Pikachu", "electric", @db)
-#
-#     pikachu_from_db = Pokemon.find(1, @db)
-#     expect(pikachu_from_db.id).to eq(1)
-#     expect(pikachu_from_db.name).to eq("Pikachu")
-#     expect(pikachu_from_db.type).to eq("electric")
-#   end
-# end
+  def alter_hp
+  end
+  
+end
